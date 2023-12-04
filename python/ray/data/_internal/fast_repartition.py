9768e30a336f9b566f354c9a81c6bfb531d15a42
Bye-legumes <121425509+Bye-legumes@users.noreply.github.com>
2023-12-05 05:19:39 +0800

    Update fast_repartition.py
    Signed-off-by: Bye-legumes <121425509+Bye-legumes@users.noreply.github.com>

--- python/ray/data/_internal/fast_repartition.py
+++ python/ray/data/_internal/fast_repartition.py
@@ -1,5 +1,4 @@
 from typing import Optional
-
 import ray
 from ray.data._internal.block_list import BlockList
 from ray.data._internal.execution.interfaces import TaskContext
@@ -12,12 +11,7 @@ from ray.data._internal.shuffle_and_partition import _ShufflePartitionOp
 from ray.data._internal.stats import DatasetStats
 from ray.data.block import BlockAccessor
 
-
-def fast_repartition(
-    blocks: BlockList,
-    num_blocks: int,
-    ctx: Optional[TaskContext] = None,
-):
+def fast_repartition(blocks: BlockList, num_blocks: int, ctx: Optional[TaskContext] = None):
     from ray.data._internal.execution.legacy_compat import _block_list_to_bundles
     from ray.data.dataset import Dataset, Schema
 
@@ -32,24 +26,25 @@ def fast_repartition(
         ),
         logical_plan=logical_plan,
     )
-    # Compute the (n-1) indices needed for an equal split of the data.
-    count = wrapped_ds.count()
-    indices = []
-    cur_idx = 0
-    for _ in range(num_blocks - 1):
-        cur_idx += count / num_blocks
-        indices.append(int(cur_idx))
-    assert len(indices) < num_blocks, (indices, num_blocks)
-    if indices:
-        splits = wrapped_ds.split_at_indices(indices)
-    else:
-        splits = [wrapped_ds]
-    # TODO(ekl) include stats for the split tasks. We may also want to
-    # consider combining the split and coalesce tasks as an optimization.
 
-    # Coalesce each split into a single block.
+    # Optimized split calculation considering block sizes
+    total_size = sum(block.size_bytes for block in blocks)
+    target_block_size = total_size / num_blocks
+    current_size = 0
+    indices = []
+    for i, block in enumerate(blocks):
+        current_size += block.size_bytes
+        if current_size >= target_block_size:
+            indices.append(i)
+            current_size = 0
+
+    # Lazy evaluation or streaming of splits
+    splits = wrapped_ds.lazy_split_at_indices(indices)
+
+    # Coalesce each split into a single block with optimized reduce task
     reduce_task = cached_remote_fn(_ShufflePartitionOp.reduce).options(num_returns=2)
 
+    # Progress bar handling
     should_close_bar = True
     if ctx is not None and ctx.sub_progress_bar_dict is not None:
         bar_name = "Repartition"
@@ -67,12 +62,11 @@ def fast_repartition(
 
     owned_by_consumer = blocks._owned_by_consumer
 
-    # Schema is safe to fetch here since we have already called
-    # get_internal_block_refs and executed the dataset.
     schema = wrapped_ds.schema(fetch_if_missing=True)
     if isinstance(schema, Schema):
         schema = schema.base_schema
-    # Early-release memory.
+
+    # Early-release memory
     del splits, blocks, wrapped_ds
 
     new_blocks, new_metadata = zip(*reduce_out)
@@ -82,36 +76,26 @@ def fast_repartition(
     if should_close_bar:
         reduce_bar.close()
 
-    # Handle empty blocks.
+    # Handle empty blocks
     if len(new_blocks) < num_blocks:
-        import pyarrow as pa
-
-        from ray.data._internal.arrow_block import ArrowBlockBuilder
-        from ray.data._internal.pandas_block import (
-            PandasBlockBuilder,
-            PandasBlockSchema,
-        )
-
-        num_empties = num_blocks - len(new_blocks)
-
-        if schema is None:
-            raise ValueError(
-                "Dataset is empty or cleared, can't determine the format of "
-                "the dataset."
-            )
-        elif isinstance(schema, pa.Schema):
-            builder = ArrowBlockBuilder()
-        elif isinstance(schema, PandasBlockSchema):
-            builder = PandasBlockBuilder()
-
-        empty_block = builder.build()
-        empty_meta = BlockAccessor.for_block(empty_block).get_metadata(
-            input_files=None, exec_stats=None
-        )  # No stats for empty block.
-        empty_blocks, empty_metadata = zip(
-            *[(ray.put(empty_block), empty_meta) for _ in range(num_empties)]
-        )
+        empty_blocks, empty_metadata = _create_empty_blocks(schema, num_blocks - len(new_blocks))
         new_blocks += empty_blocks
         new_metadata += empty_metadata
 
     return BlockList(new_blocks, new_metadata, owned_by_consumer=owned_by_consumer), {}
+
+def _create_empty_blocks(schema, num_empties):
+    import pyarrow as pa
+    from ray.data._internal.arrow_block import ArrowBlockBuilder
+    from ray.data._internal.pandas_block import PandasBlockBuilder, PandasBlockSchema
+
+    if schema is None:
+        raise ValueError("Dataset is empty or cleared, can't determine the format of the dataset.")
+    elif isinstance(schema, pa.Schema):
+        builder = ArrowBlockBuilder()
+    elif isinstance(schema, PandasBlockSchema):
+        builder = PandasBlockBuilder()
+
+    empty_block = builder.build()
+    empty_meta = BlockAccessor.for_block(empty_block).get_metadata(input_files=None, exec_stats=None)
+    return zip(*[(ray.put(empty_block), empty_meta) for _ in range(num_empties)])
